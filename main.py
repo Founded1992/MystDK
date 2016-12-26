f61a092b337c731e901ae9baf13d7a3a0be100b1
nikita-cvrobotics <171515@cvsd356.org>
2016-12-26 11:48:28 -0800

    TRLSlide is functional
    Navigation up and down hall is... shnazzy.

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from __future__ import division
 from Tkinter import *
 from tkSnack import *
 from PIL import Image, ImageTk
+from threading import Thread
 import time
+import sys
 
 root = Tk()
 
 def close(evt):
     root.destroy()
-
+    
 root.title("Cube Island")
 root.bind("<Escape>", close)
 sw, sh = root.winfo_screenwidth(), root.winfo_screenheight()
@@ -23,54 +25,113 @@ ThemeLength = 184000
 initializeSnack(root)
 s = Sound()
 s.read('Thief in the Night.mp3')
-def playTheme():
+def playThemeSmooth():
+    while True:
+        s.play()
+        time.sleep(ThemeLength)
+t = Thread(target=playThemeSmooth)
+#t.start()
+
+'''def playTheme():
     s.play()
     root.after(ThemeLength, playTheme)
-playTheme()
+playTheme()'''
 
 currScene = None
+d = None
 
 class SceneBasic():
     def __init__(self, img, clkZns):
         self.image = img
         self.clickZones = clkZns
+    def getImg(self):
+        return self.image
 class ClickZone():
-    def __init__(self, x, y, w, h, nextScene):
+    def __init__(self, x, y, w, h, TRL):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.nextScene = nextScene
+        self.TRL = TRL
+class TRLBasic():
+    def __init__(self, targetString):
+        self.targetString = targetString
+    def action(self):
+        d.BasicSelect(self.targetString)
+class TRLSlide():
+    def __init__(self, targetString, direction):
+        self.targetString = targetString
+        self.direction = direction
+    def action(self):
+        d.slideImg(self.targetString, self.direction)
+        d.BasicSelect(self.targetString)
 class drawer():
-    def BasicSelect(self, targetScene):
-        global currScene
-        currScene = world[targetScene]
-        
-        im = Image.open(currScene.image)
+    def slideImg(self, targetStr, direction):
+        im = Image.open(world[targetStr].getImg())
         imw = im.size[0]
         imh = im.size[1]
         scale = 1
-        currScene.offx = 0
-        currScene.offy = 0
         if imw/imh < sw/sh: #If image is taller than screen
             #set scale by height, not width
             scale = sh/imh
-            currScene.offx = int( (sw-(scale*imw)) / 2)
         else:
             #set scale by width, not height
             scale = sw/imw
-            currScene.offy = int( (sh-(scale*imh)) / 2)
+        imw = int(scale*imw)
+        imh = int(scale*imh)
+        im = im.resize((imw, imh), Image.ANTIALIAS)
+        self.impsl2 = ImageTk.PhotoImage(im)
+        sloffx = 0
+        sloffy = 0
+        incsox = 0
+        incsoy = 0
+        if direction == 0:
+            sloffy = imh
+            incsoy = imh / -16
+        elif direction == 1:
+            sloffx = imw * -1
+            incsox = imw / 16
+        elif direction == 2:
+            sloffy = imh * -1
+            incsoy = imh / 16
+        else:
+            sloffx = imw
+            incsox = imw / -16
+        self.slScHndl = c.create_image((sw/2 + sloffx, sh/2 + sloffy), image=self.impsl2)
+        for i in range(0, 16):
+            currSloffx = int(sloffx * ((16-i)/16))
+            currSloffy = int(sloffy * ((16-i)/16))
+            c.move(self.slScHndl, incsox, incsoy)
+            c.move(self.currScHndl, incsox, incsoy)
+            c.update()
+            #print("cake awesome cake")
+            time.sleep(0.01)
+    def BasicSelect(self, targetScene):
+        global currScene
+        currScene = world[targetScene]
+        im = Image.open(currScene.getImg())
+        imw = im.size[0]
+        imh = im.size[1]
+        scale = 1
+        if imw/imh < sw/sh: #If image is taller than screen
+            #set scale by height, not width
+            scale = sh/imh
+        else:
+            #set scale by width, not height
+            scale = sw/imw
         currScene.imw = int(scale*imw)
         currScene.imh = int(scale*imh)
+        currScene.offx = (sw - currScene.imw) / 2
+        currScene.offy = (sh - currScene.imh) / 2
         im = im.resize((currScene.imw, currScene.imh), Image.ANTIALIAS)
         self.imp = ImageTk.PhotoImage(im)
-        c.create_image((sw/2, sh/2), image=self.imp)
+        self.currScHndl = c.create_image((sw/2, sh/2), image=self.imp)
         
 world = {
-    "cube_room":SceneBasic("RenderPictures/Cube_Y_G_Face.png", [ClickZone(0, 0, 0.1, 1, "hallway_code"), ClickZone(0.9, 0, 0.1, 1, "hallway_code")]),
-    "hallway_code":SceneBasic("RenderPictures/Hallway_Code.png", [ClickZone(0, 0, 0.1, 1, "cube_room"), ClickZone(0.9, 0, 0.1, 1, "cube_room"), ClickZone(0.3, 0, 0.4, 1, "code_room")]),
-    "code_room":SceneBasic("RenderPictures/Code-Room.png", [ClickZone(0, 0, 0.1, 1, "hallway_cube"), ClickZone(0.9, 0, 0.1, 1, "hallway_cube")]),
-    "hallway_cube":SceneBasic("RenderPictures/Hallway_Cube_Y_G_Face.png", [ClickZone(0, 0, 0.1, 1, "code_room"), ClickZone(0.9, 0, 0.1, 1, "code_room"), ClickZone(0.3, 0, 0.4, 1, "cube_room")]),
+    "cube_room":SceneBasic("RenderPictures/Cube_Y_G_Face.png", [ClickZone(0, 0, 0.1, 1, TRLSlide("hallway_code", 1)), ClickZone(0.9, 0, 0.1, 1, TRLSlide("hallway_code", 3))]),
+    "hallway_code":SceneBasic("RenderPictures/Hallway_Code.png", [ClickZone(0, 0, 0.1, 1, TRLSlide("cube_room", 1)), ClickZone(0.9, 0, 0.1, 1, TRLSlide("cube_room", 3)), ClickZone(0.3, 0, 0.4, 1, TRLBasic("code_room"))]),
+    "code_room":SceneBasic("RenderPictures/Code-Room.png", [ClickZone(0, 0, 0.1, 1, TRLSlide("hallway_cube", 1)), ClickZone(0.9, 0, 0.1, 1, TRLSlide("hallway_cube", 3))]),
+    "hallway_cube":SceneBasic("RenderPictures/Hallway_Cube_Y_G_Face.png", [ClickZone(0, 0, 0.1, 1, TRLSlide("code_room", 1)), ClickZone(0.9, 0, 0.1, 1, TRLSlide("code_room", 3)), ClickZone(0.3, 0, 0.4, 1, TRLBasic("cube_room"))]),
 }
 
 d = drawer()
@@ -81,7 +142,7 @@ def clickHandle(e):
     sceney = (e.y - currScene.offy) / currScene.imh
     for i in currScene.clickZones:
         if scenex > i.x and scenex < (i.x + i.w) and sceney > i.y and sceney < (i.y + i.h):
-            d.BasicSelect(i.nextScene)
+            i.TRL.action()
         
 root.bind("<Button 1>", clickHandle)
 
